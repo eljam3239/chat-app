34266e03f34fc78ec3a91921aa6bac193f40fd59
Elijah James <elijames2301@gmail.com>
2025-07-29 10:25:24 -0400

    Add logout to chat.dart

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,15 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FluttterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Logged in!'),
-      ),
+      body: Center(child: Text('Logged in!')),
     );
   }
 }
