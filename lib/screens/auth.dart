4f5060bd2072a5292fe5a4e9c6e2148972a1c027
Elijah James <elijames2301@gmail.com>
2025-07-29 13:59:03 -0400

    Add username to create user screen and db

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -25,6 +25,7 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
   File? _selectedImage;
   var _isAuthenticating = false;
+  var _enteredUsername = '';
 
   void _submit() async {
     final isValid = _form.currentState!.validate();
@@ -64,7 +65,7 @@ class _AuthScreenState extends State<AuthScreen> {
             .collection('users')
             .doc(userCredentials.user!.uid)
             .set({
-              'username': 'to be done',
+              'username': _enteredUsername,
               'email': _enteredEmail,
               'image_url': imageUrl,
             });
@@ -135,6 +136,20 @@ class _AuthScreenState extends State<AuthScreen> {
                               _enteredEmail = value!;
                             },
                           ),
+                          if (!_isLogin)
+                            TextFormField(
+                              decoration: InputDecoration(labelText: 'Username'),
+                              enableSuggestions: false,
+                              validator: (value) {
+                                if (value == null || value.isEmpty || value.trim().length <4) {
+                                  return 'Please enter at least 4 characters.';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredUsername = value!;
+                              },
+                            ),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Password',
